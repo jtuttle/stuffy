3a8bea57a8b780ee24134fdf9fb8ae6126ed18a7
John Tuttle <jtuttle@craw.cc>
2014-10-08 23:47:10 -0400

    vertex linking/unlinking

---
+++ Stuffy/Assets/Editor/DynamicVertexEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(DynamicVertex))]
+public class DynamicVertexEditor : Editor {
+    public override void OnInspectorGUI () {
+        if((target as DynamicVertex).LinkedVertex != null) {
+            if(GUILayout.Button("Break Vertex Link"))
+                BreakVertexLink();
+        }
+
+        base.OnInspectorGUI();
+	}
+
+    private void BreakVertexLink() {
+        DynamicVertex linkedVertex = (target as DynamicVertex).LinkedVertex;
+
+        linkedVertex.LinkedVertex = null;
+        (target as DynamicVertex).LinkedVertex = null;
+    }
+}
--- Stuffy/Assets/Scripts/DynamicVertex.cs
+++ Stuffy/Assets/Scripts/DynamicVertex.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class DynamicVertex : MonoBehaviour {
-    public Vector2 Normal = new Vector3(0, 1);
+    public Vector2 Normal = new Vector3(0, 0, 1);
+
+    public DynamicVertex LinkedVertex;
+
+    public DynamicMesh ParentMesh { get; private set; }
+
+    private Vector3 _lastPosition;
 
 	void Awake() {
-		
+        ParentMesh = transform.parent.GetComponent<DynamicMesh>();
 	}
 
 	void Update() {
-	
+        if(LinkedVertex != null) {
+            if(LinkedVertex.LinkedVertex == null)
+                LinkedVertex.LinkedVertex = this;
+
+            if(transform.position != _lastPosition)
+                LinkedVertex.transform.position = transform.position;
+        }
+
+        _lastPosition = transform.position;
 	}
 }
