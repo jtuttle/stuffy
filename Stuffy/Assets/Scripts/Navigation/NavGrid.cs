0179ddde237bba07a1dcd608103f628708e12289
John Tuttle <jtuttle@craw.cc>
2014-10-29 08:26:38 -0400

    remove mesh link logic

--- Stuffy/Assets/Scripts/Navigation/NavGrid.cs
+++ Stuffy/Assets/Scripts/Navigation/NavGrid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NavGridNode {
     public readonly Vector2 Data;
@@ -27,14 +28,14 @@ public class NavGrid {
     private Rect _area;
     private int _interval;
 
-    public NavGrid(Rect area, int interval) {
+    public NavGrid(Rect area, List<DynamicMesh> meshes, int interval) {
         _area = area;
         _interval = interval;
 
-        CreateGrid();
+        CreateGrid(meshes);
     }
 
-    private void CreateGrid() {
+    private void CreateGrid(List<DynamicMesh> meshes) {
         int width = (int)(_area.width / _interval);
         int height = (int)(_area.height / _interval);
         
@@ -42,12 +43,25 @@ public class NavGrid {
 
         float startX = _area.xMin + (_interval / 2.0f);
         float startY = _area.yMin + (_interval / 2.0f);
-        
+
+        // OPT - I'm sure this can be optimized. At the very least, you can slap a square
+        // around each grid and only consider points in the squares instead of looping over 
+        // the entire area.
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
-                Vector2 data = new Vector2(startX + (x * _interval), startY + (y * _interval));
-                Nodes[x, y] = new NavGridNode(data);
+                Vector2 point = new Vector2(startX + (x * _interval), startY + (y * _interval));
+
+                if(PointInMeshes(point, meshes))
+                    Nodes[x, y] = new NavGridNode(point);
             }
         }
+    }
+
+    private bool PointInMeshes(Vector2 point, List<DynamicMesh> meshes) {
+        foreach(DynamicMesh mesh in meshes) {
+            if(mesh.PointInMesh(point))
+                return true;
+        }
+        return false;
     }
 }
--- Stuffy/Assets/Scripts/Navigation/NavigationSystem.cs
+++ Stuffy/Assets/Scripts/Navigation/NavigationSystem.cs
@@ -4,26 +4,14 @@ using System.Collections.Generic;
 
 [ExecuteInEditMode]
 public class NavigationSystem : MonoBehaviour {
+    public GameObject Player;
     public GameObject Scene;
 
     private List<DynamicMesh> _meshes;
     private NavGrid _navGrid;
 
-    //private List<MeshLink> _meshLinks;
-
     void Awake() {
-        // Collect existing meshes and add link listeners.
         _meshes = new List<DynamicMesh>(gameObject.GetComponentsInChildren<DynamicMesh>());
-
-        /*
-        foreach(DynamicMesh mesh in _meshes) {
-            mesh.OnVertexLinkCreated += HandleVertexLinkCreated;
-            mesh.OnVertexLinkDestroyed += HandleVertexLinkDestroyed;
-        }
-        */
-
-        // Collect existing mesh links.
-        //_meshLinks = new List<MeshLink>(gameObject.GetComponentsInChildren<MeshLink>());
     }
 
 	void Update() {
@@ -32,20 +20,13 @@ public class NavigationSystem : MonoBehaviour {
 
     public void AddMesh() {
         DynamicMesh newMesh = CreateNewMesh();
-        
-        //newMesh.OnVertexLinkCreated += HandleVertexLinkCreated;
-        //newMesh.OnVertexLinkDestroyed += HandleVertexLinkDestroyed;
-
         _meshes.Add(newMesh);
     }
     
     public void RemoveMesh() {
         DynamicMesh lastMesh = _meshes[_meshes.Count - 1];
         _meshes.Remove(lastMesh);
-        
-        //lastMesh.OnVertexLinkCreated -= HandleVertexLinkCreated;
-        //lastMesh.OnVertexLinkDestroyed -= HandleVertexLinkDestroyed;
-        
+
         GameObject.DestroyImmediate(lastMesh.gameObject);
     }
 
@@ -63,14 +44,13 @@ public class NavigationSystem : MonoBehaviour {
     public void RefreshGrid() {
         GameObject.DestroyImmediate(GameObject.Find("NavGridPoints"));
 
-        Rect area = new Rect(-Scene.transform.localScale.x / 2,
-                             -Scene.transform.localScale.y / 2,
-                             Scene.transform.localScale.x,
-                             Scene.transform.localScale.y);
+        Bounds sceneBounds = Scene.GetComponent<tk2dSprite>().GetBounds();
+        Rect area = new Rect(sceneBounds.min.x, sceneBounds.min.y,
+                             sceneBounds.size.x, sceneBounds.size.y);
 
-        int interval = 60;
+        int interval = 50;
 
-        _navGrid = new NavGrid(area, interval);
+        _navGrid = new NavGrid(area, _meshes, interval);
 
         VisualizeNavGrid();
     }
@@ -84,7 +64,7 @@ public class NavigationSystem : MonoBehaviour {
             for(int x = 0; x < _navGrid.Nodes.GetLength(0); x++) {
                 NavGridNode node = _navGrid.Nodes[x,y];
 
-                if(PointInMesh(node.Data)) {
+                if(node != null) {
                     GameObject point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                     point.transform.parent = navGridPoints.transform;
                     point.transform.localPosition = new Vector3(node.Data.x, node.Data.y, -50);
@@ -102,95 +82,4 @@ public class NavigationSystem : MonoBehaviour {
 
         return false;
     }
-
-    /*
-    private void CreateNavGrid() {
-        GameObject navGridPoints = new GameObject("NavGridPoints");
-        navGridPoints.transform.parent = gameObject.transform;
-        navGridPoints.transform.localPosition = Vector3.zero;
-
-        int size = 80;
-
-        int width = (int)(Scene.transform.localScale.x / size);
-        int height = (int)(Scene.transform.localScale.y / size);
-
-        float startX = -(Scene.transform.localScale.x / 2) + (size / 2.0f);
-        float startY = -(Scene.transform.localScale.y / 2) + (size / 2.0f);
-
-        _navGrid = new GameObject[width, height];
-
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                GameObject point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                point.transform.parent = navGridPoints.transform;
-                point.transform.localPosition = new Vector3(startX + (x * size), startY + (y * size), -50);
-                point.transform.localScale = new Vector3(10, 10, 10);
-
-                _navGrid[x, y] = point;
-            }
-        }
-    }
-    */
-
-    /*
-    private void HandleVertexLinkCreated(DynamicVertex v1, DynamicVertex v2) {
-        Debug.Log("link created");
-
-        VertexLink vertexLink = new VertexLink(v1, v2);
-        MeshLink meshLink = GetExistingMeshLinkForVertexLink(vertexLink);
-
-        // No link exists between the two meshes.
-        if(meshLink == null)
-            meshLink = CreateNewMeshLink();
-
-        // Link is already recorded.
-        if(meshLink.HasExactVertexLink(vertexLink))
-            return;
-
-        // User tries to create a third link between two meshes.
-        if(meshLink.IsFull()) {
-            Debug.LogError("Can only link two pairs of vertices between a mesh.");
-            v1.BreakVertexLink();
-            return;
-        }
-
-        meshLink.AddVertexLink(vertexLink);
-        _meshLinks.Add(meshLink);
-    }
-
-    private MeshLink GetExistingMeshLinkForVertexLink(VertexLink vertexLink) {
-        foreach(MeshLink meshLink in _meshLinks) {
-            if(meshLink.HasMatchingVertexLink(vertexLink))
-                return meshLink;
-        }
-
-        return null;
-    }
-
-    private void HandleVertexLinkDestroyed(DynamicVertex v1, DynamicVertex v2) {
-        Debug.Log("link destroyed");
-
-        VertexLink vertexLink = new VertexLink(v1, v2);
-        MeshLink meshLink = GetExistingMeshLinkForVertexLink(vertexLink);
-
-        if(meshLink != null) {
-            meshLink.RemoveVertexLink(vertexLink);
-
-            if(meshLink.IsEmpty()) {
-                _meshLinks.Remove(meshLink);
-                GameObject.DestroyImmediate(meshLink.gameObject);
-            }
-        }
-    }
-
-    private MeshLink CreateNewMeshLink() {
-        GameObject prototype = (GameObject)Resources.Load("Prefabs/MeshLink");
-
-        GameObject go = (GameObject)GameObject.Instantiate(prototype);
-        go.transform.parent = gameObject.transform;
-        go.name = "MeshLink" + (_meshLinks.Count + 1);
-        
-        return go.GetComponent<MeshLink>();
-    }
-    */
 }
